fdcc81f740c774edeb5dcae11cf587ee7ad61a5c
root <root@wang.(none)>
2015-11-13 09:36:07 +0800

    up remot

--- remot/remot_upload.py
+++ remot/remot_upload.py
@@ -20,10 +20,12 @@ from subprocess import call
 #######################################
 #  这个函数是上传文件
 #######################################
-def upload_remote(remote_file_path, local_file_path, ip, port, user, passwd, mv, cmd):
+def upload_remote(remote_file_path, local_file_path, ip, port, user, passwd, mkdir_catalog, mv, cmd):
+	if mv:
+		command(ip, port, user, passwd, mv)
+	if mkdir_catalog:
+		command(ip, port, user, passwd, mkdir_catalog)
 	try:
-		if mv:
-			command(ip, port, user, passwd, mv)
 		up = paramiko.Transport(ip, int(port))
 		up.connect(username = user, password = passwd)
 		sftp = paramiko.SFTPClient.from_transport(up)
@@ -31,11 +33,12 @@ def upload_remote(remote_file_path, local_file_path, ip, port, user, passwd, mv,
 		local_path = local_file_path #本地要上传的文件
 		sftp.put(local_path, remote_path)
 		up.close()
-		print("%s Upload Ok\n" % ip)
-		if cmd:
-			command(ip, port, user, passwd, cmd)
+		print("%s Upload Ok,\n" % ip)
 	except:
 		print("%s Upload Error\n" % ip)
+		Error_log(ip, [], "upload error")
+	if cmd:
+		command(ip, port, user, passwd, cmd)
 
 ######################################
 #   这个函数是负责远程执行shell命令
@@ -45,35 +48,47 @@ def command(ip, port, username, passwd, cmd):
 		com = paramiko.SSHClient()
 		com.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		com.connect(ip, int(port), username, passwd, timeout=5)
-		stdin, stdout, stderr = com.exec_command(cmd)
-		filename_list = stdout.readlines()
-		stdin.write("Y")
+		for cmd_ in cmd:
+			stdin, stdout, stderr = com.exec_command(cmd_)
+			stdin.write("Y")
+			filename_list = stdout.readlines()
 		com.close()
 		print("%s\tCommand Executive OK\n" % ip)
 	except:
-		print("%s\tCommand Executive Error\n" % ip)
+		print("%s\t %s Command Error\n" % ip, cmd)
+		Error_log(ip, cmd, "command error")
 
+def Error_log(ip, command, reason):
+	error_file = './error.log'
+	com = " ".join(command)
+	time_ = time.strftime("%Y-%m-%d %H:%M:%S") + "\n"
+	log_ = ("%s %s %s" % (ip, com, reason)) + "\n\n"
+	f = open(error_file, 'a+')
+	f.write(time_)
+	f.write(log_)
+	f.close()
 
 if __name__=="__main__":
-	remote_path = '/usr/local/test.tar.gz'
+	remote_path = '/zywa/test.tar.gz'
 	local_path = '/usr/local/test.tar.gz'
-	cd_path = "cd /usr/local/ ;"
-	tar = "tar -zxvf"
-	file = "test.tar.gz"
-	cmd = "%s %s %s" % (cd_path, tar, file)
-	mv_ngx = "mv test test_bak ;"
-	mv_ngx_gz = "mv test.tar.gz test.tar.gz.bak"
-	mv = "%s %s %s" % (cd_path, mv_ngx, mv_ngx_gz)
+	mkdir_catalog = ['cd / ; mkdir zywa']
+	cmd = ['cd /zywa/ ; \
+			tar -zxvf test.tar.gz ;'
+		  ]
+	mv = ['cd /zywa/ ; mv test test_bak ; \
+		  mv test.tar.gz test.tar.gz.bak ;'
+		 ]
+	
 	hosts = open("linux_server.list")
 	threads = []   #使用线程
 	for host in hosts:
 		if host:
 			ip, user, passwd, port = host.split(":")
-			threads.append(threading.Thread(target = upload_remote, args = (remote_path, local_path, ip, port, user, passwd, mv, cmd)))
+			threads.append(threading.Thread(target = upload_remote, args = (remote_path, local_path, ip, port, user, passwd, mkdir_catalog, mv, cmd)))
 			
 	for t in threads:
+		t.setDaemon(True)
 		t.start() #启动所有线程
-	
 	for t in threads:
-		t.join() #主线程中等待所有子线程退出
+		t.join() #主(父)线程中等待所有子线程退出
 
